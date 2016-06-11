2ee3386f82a6c27235d521da5dea6b83ca8be854
oren <get.oren@gmail.com>
2016-06-10 23:12:30 -0700

    Work-in-progress: fixedToSymYear

--- bundle.js
+++ bundle.js
@@ -1,4 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
 var helpers = {
   quotient: function (x, y) {
     return Math.floor(x / y);
@@ -35,7 +36,9 @@ var helpers = {
   }
 };
 module.exports = helpers;
+global.helpers = module.exports;
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],2:[function(require,module,exports){
 (function (global){
 // @TODO watch
